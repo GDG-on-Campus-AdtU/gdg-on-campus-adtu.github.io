ae6af63d651a8bda9443123263c43eaacf1381d6
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-01-29 16:38:17 +0530

    Add menubar component and setup mode watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import "../app.css";
+  import { ModeWatcher } from "mode-watcher";
 
   let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<div class="relative min-h-screen">
+  {@render children()}
+</div>
